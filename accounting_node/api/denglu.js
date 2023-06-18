4b6ff90b39f06dae35d59925b56404ca4a9449ae
袁隆成 <756885686@qq.com>
2023-06-18 19:07:13 +0800

    token

--- accounting_node/api/denglu.js
+++ accounting_node/api/denglu.js
@@ -21,10 +21,10 @@ Router.post('/login', async (req, res) => {    //登录接口
     }
     // 存在
     // 根据用户数据，生成token
-    //将账号密码信息存入对象，转换为token
-    let obj = { username: row[0].username, password: row[0].password }
+    //将id账号密码信息存入对象，转换为token
+    let obj = { id: row[0].id, username: row[0].username, password: row[0].password }
     const token = JWT.sign(obj, 'hello', {
-        expiresIn: '7d',
+        expiresIn: '7d',   //过期时间
     });
     res.send({
         code: 200,
@@ -33,4 +33,26 @@ Router.post('/login', async (req, res) => {    //登录接口
         token: token
     })
 })
+
+// 校验登录
+router.post("/check_login", async (req, res) => {
+    // 从token获取用户id
+    const token = req.body.token;
+    try {
+        const { _id } = JWT.verify(token, 'hello');
+        //token没问题
+        const result = await User.findById(_id).populate('roles');
+        if (result) {
+            //登录的状态是正确的。
+            res.json({ code: 0, message: "登录成功", data: result });
+        } else {
+            //登录状态被窜改了。
+            res.json({ code: -1, message: "过期了,请重新登录" });
+        }
+
+    } catch (error) {
+        //token失效了
+        res.json({ code: -1, message: "过期了,请重新登录" });
+    }
+});
 
