8bcbd56214b3825918c45cdac75468cfe1f18c87
袁隆成 <756885686@qq.com>
2023-06-19 18:06:27 +0800

    路由守卫

--- accounting_electron/src/router/index.ts
+++ accounting_electron/src/router/index.ts
@@ -1,15 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: routes,
 })
 
-router.beforeEach((to, from, next) => {
-    // 继续前进 next()
-    // 返回 false 以取消导航
-    next()
+NProgress.configure({
+    easing: 'ease',   // 动画方式
+    speed: 1000,   // 递增进度条的速度
+    showSpinner: false,   // 是否显示加载ico
+    trickleSpeed: 200,   // 自动递增间隔
+    minimum: 0.3,   // 更改启动时使用的最小百分比
+    parent: 'body',   //指定进度条的父容器
+})
+
+router.beforeEach((to: any, from: any, next: any) => {
+    NProgress.start()
+    if (to.path == "/login") {
+        next()
+    } else {
+        const token = localStorage.getItem("token")
+        if (token) {
+            next()
+        } else {
+            next({ path: "/login" })
+        }
+    }
+})
+
+router.afterEach((to: any, from: any) => {
+    NProgress.done(true)
 })
 
 export default router
