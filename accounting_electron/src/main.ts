86bbd905ba11f5370d28851030c40a8624265cd2
袁隆成 <756885686@qq.com>
2023-07-01 11:28:33 +0800

    Symbol方式引用iconfont

--- accounting_electron/src/main.ts
+++ accounting_electron/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from '@/router'
 
 import '@/style/reset.css'
-import "@/style/iconfont/iconfont.css";
+import "@/style/iconfont/iconfont.js";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App).use(router)
