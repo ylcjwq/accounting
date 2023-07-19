7022110e1626b23a5e7c58b686bcdc9cff84b5e5
袁隆成 <756885686@qq.com>
2023-07-19 18:05:37 +0800

    修改密码的校验规则

--- accounting_electron/src/view/user/com/basicInfo.vue
+++ accounting_electron/src/view/user/com/basicInfo.vue
@@ -9,36 +9,43 @@
       <el-tab-pane label="基本资料" name="first">
         <el-form
           ref="ruleFormRef"
-          :model="ruleForm"
-          :rules="rules"
+          :model="ruleFormBasic"
+          :rules="rulesBasic"
           label-width="80px"
           class="demo-ruleForm"
-          :size="formSize"
           status-icon
         >
           <el-form-item label="用户名称" prop="name">
-            <el-input v-model="ruleForm.name" />
+            <el-input v-model="ruleFormBasic.name" />
           </el-form-item>
         </el-form>
       </el-tab-pane>
       <el-tab-pane label="修改密码" name="second">
         <el-form
           ref="ruleFormRef"
-          :model="ruleForm"
-          :rules="rules"
+          :model="ruleFormPassword"
+          :rules="rulesPassword"
           label-width="80px"
           class="demo-ruleForm"
-          :size="formSize"
           status-icon
         >
-          <el-form-item label="旧密码" prop="name">
-            <el-input v-model="ruleForm.name" />
+          <el-form-item label="旧密码" prop="oldPassword">
+            <el-input
+              v-model="ruleFormPassword.oldPassword"
+              placeholder="请输入旧密码"
+            />
           </el-form-item>
-          <el-form-item label="新密码" prop="name">
-            <el-input v-model="ruleForm.name" />
+          <el-form-item label="新密码" prop="newPassword">
+            <el-input
+              v-model="ruleFormPassword.newPassword"
+              placeholder="请输入新密码"
+            />
           </el-form-item>
-          <el-form-item label="确认密码" prop="name">
-            <el-input v-model="ruleForm.name" />
+          <el-form-item label="确认密码" prop="surePassword">
+            <el-input
+              v-model="ruleFormPassword.surePassword"
+              placeholder="请再次输入新密码"
+            />
           </el-form-item>
         </el-form>
       </el-tab-pane>
@@ -53,26 +60,100 @@
 import { reactive, ref } from "vue";
 import type { FormInstance, FormRules, TabsPaneContext } from "element-plus";
 
-interface RuleForm {
+interface RuleFormBasic {
   name: string;
 }
 
-const formSize = ref("default");
+interface RuleFormPassword {
+  oldPassword: string;
+  newPassword: string;
+  surePassword: string;
+}
+
 const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive<RuleForm>({
+//基本信息
+const ruleFormBasic = reactive<RuleFormBasic>({
   name: "",
 });
+//修改密码
+const ruleFormPassword = reactive<RuleFormPassword>({
+  oldPassword: "",
+  newPassword: "",
+  surePassword: "",
+});
 
-const activeName = ref("first");
+const activeName = ref<string>("first");
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event);
 };
 
-const rules = reactive<FormRules<RuleForm>>({
+//基本信息校验规则
+const rulesBasic = reactive<FormRules<RuleFormBasic>>({
   name: [
-    { required: true, message: "Please input Activity name", trigger: "blur" },
-    { min: 3, max: 5, message: "Length should be 3 to 5", trigger: "blur" },
+    { required: true, message: "请输入用户名称", trigger: "blur" },
+    { min: 2, max: 8, message: "用户名称必须为2~8位字符", trigger: "blur" },
+  ],
+});
+
+//修改密码校验规则
+const rulesPassword = reactive<FormRules<RuleFormPassword>>({
+  oldPassword: [
+    { required: true, message: "请输入旧密码", trigger: "blur" },
+    { min: 6, max: 12, message: "密码必须为6~12位字符", trigger: "blur" },
+    {
+      pattern: /^[a-zA-Z0-9@.~!#$%^]*$/,
+      message: "密码只能包含字母、数字和特殊字符",
+      trigger: "blur",
+    },
+  ],
+  newPassword: [
+    { required: true, message: "请输入新密码", trigger: "blur" },
+    { min: 6, max: 12, message: "密码必须为6~12位字符", trigger: "blur" },
+    {
+      pattern: /^[a-zA-Z0-9@.~!#$%^]*$/,
+      message: "密码只能包含字母、数字和特殊字符",
+      trigger: "blur",
+    },
+    {
+      validator: (_rule, value, callback) => {
+        if (value === ruleFormPassword.oldPassword) {
+          callback(new Error("新密码不能与旧密码一致"));
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur",
+    },
+  ],
+  surePassword: [
+    { required: true, message: "请再次输入新密码", trigger: "blur" },
+    { min: 6, max: 12, message: "密码必须为6~12位字符", trigger: "blur" },
+    {
+      pattern: /^[a-zA-Z0-9@.~!#$%^]*$/,
+      message: "密码只能包含字母、数字和特殊字符",
+      trigger: "blur",
+    },
+    {
+      validator: (_rule, value, callback) => {
+        if (value === ruleFormPassword.oldPassword) {
+          callback(new Error("新密码不能与旧密码一致"));
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur",
+    },
+    {
+      validator: (_rule, value, callback) => {
+        if (value !== ruleFormPassword.newPassword) {
+          callback(new Error("两次输入的新密码不一致"));
+        } else {
+          callback();
+        }
+      },
+      trigger: "blur",
+    },
   ],
 });
 </script>
