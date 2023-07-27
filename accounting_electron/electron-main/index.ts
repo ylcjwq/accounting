28e1304cd1db1da0bb2ba7e82f888f9cb8d2f83d
袁隆成 <756885686@qq.com>
2023-07-27 16:44:46 +0800

    wip

--- accounting_electron/electron-main/index.ts
+++ accounting_electron/electron-main/index.ts
@@ -2,44 +2,47 @@ import { app, BrowserWindow, session } from "electron";
 import path from "path";
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        webPreferences: {
-            contextIsolation: false,  //是否隔离上下文
-            nodeIntegration: true, //渲染进程使用Node API
-            preload: path.join(__dirname, "../../electron-preload/index.js"), //需要引用的js文件,electron还不能识别ts
-        }
-    })
+  const win = new BrowserWindow({
+    webPreferences: {
+      contextIsolation: false, //是否隔离上下文
+      nodeIntegration: true, //渲染进程使用Node API
+      preload: path.join(__dirname, "../../electron-preload/index.js"), //需要引用的js文件,electron还不能识别ts
+    },
+  });
 
-    //如果打包了，渲染index.html
-    if (app.isPackaged) {
-        win.loadFile(path.join(__dirname, "../../index.html"))
-    } else {
-        let url = "http://localhost:3000"
-        win.loadURL(url)
-    }
-}
+  //如果打包了，渲染index.html
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, "../../index.html"));
+  } else {
+    let url = "http://localhost:3000";
+    win.loadURL(url);
+  }
+};
 
 app.whenReady().then(() => {
-    createWindow()   //创建窗口
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    })
-})
+  createWindow(); //创建窗口
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.once("ready", async () => {   //给electron添加vue调试工具
-    let extensionPath = "C:\\Users\\Administrator\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Extensions\\olofadcdnkkjdfgjcmjaadnlehnnihnl\\6.5.0_0"
-    // BrowserWindow.addDevToolsExtension(extensionPath)
-    try {
-        await session.defaultSession.loadExtension(extensionPath)
-    } catch (error) {
-        console.log(error);
-    }
-
-})
+app.once("ready", async () => {
+  //给electron添加vue调试工具
+  let extensionPath =
+    "C:\\Users\\Administrator\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Extensions\\olofadcdnkkjdfgjcmjaadnlehnnihnl\\6.5.0_0";
+  // let extensionPath =
+  //   "C:\\Users\\ylc\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Extensions\\olofadcdnkkjdfgjcmjaadnlehnnihnl\\6.5.0_0";
+  // BrowserWindow.addDevToolsExtension(extensionPath)
+  try {
+    await session.defaultSession.loadExtension(extensionPath);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //关闭窗口
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit()
-    }
-})
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
--- accounting_electron/vite.config.ts
+++ accounting_electron/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 import * as path from "path";
 import electron from "vite-plugin-electron";
 import electronRenderer from "vite-plugin-electron/renderer";
 import polyfillExports from "vite-plugin-electron/polyfill-exports";
-
 
 export default defineConfig({
   plugins: [
@@ -35,22 +34,20 @@ export default defineConfig({
     emptyOutDir: false, // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录
   },
   resolve: {
-    dedupe: [
-      'vue'
-    ],
+    dedupe: ["vue"],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      "@": path.join(__dirname, "src"),
+    },
   },
   server: {
-    proxy: { //配置代理服务器
+    proxy: {
+      //配置代理服务器
       "/api": {
-        target: "http://8.130.71.186:3300",    //服务器url
-        // target: "http://localhost:3300",        //本地url 
-        changeOrigin: true,    //允许跨域
-        rewrite: (path) => path.replace(/^\/api/, ""),     //重写路径,替换/api
+        target: "http://8.130.71.186:3300", //服务器url
+        // target: "http://localhost:3300", //本地url
+        changeOrigin: true, //允许跨域
+        rewrite: (path) => path.replace(/^\/api/, ""), //重写路径,替换/api
       },
     },
   },
 });
-
