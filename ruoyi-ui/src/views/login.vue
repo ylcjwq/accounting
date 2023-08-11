69ac43d3978e3c61852edc6eba7dd5822025bdd7
luo xiaoli <15362175892@163.com>
2023-08-11 17:39:25 +0800

    提交：部署测试

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -50,7 +50,7 @@
           <span v-else>登 录 中...</span>
         </el-button>
         <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
+          <router-link class="link-type" :to="'/register'">push代码</router-link>
         </div>
       </el-form-item>
     </el-form>
