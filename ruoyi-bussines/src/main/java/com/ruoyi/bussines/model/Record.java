ce715152722a5708099acf177a4d7d4733433aee
luo xiaoli <15362175892@163.com>
2023-08-22 18:22:05 +0800

    new：优化收入/支出时间格式

--- ruoyi-bussines/src/main/java/com/ruoyi/bussines/model/Record.java
+++ ruoyi-bussines/src/main/java/com/ruoyi/bussines/model/Record.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Record {
     private Integer region;
     private Integer number;
     private String remark;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date time;
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
