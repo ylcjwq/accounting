e4536a2283f9ae811bae7fa4000359de970780e0
袁隆成 <756885686@qq.com>
2023-08-24 18:33:04 +0800

    报表页表格

--- accounting_electron/src/view/report/index.vue
+++ accounting_electron/src/view/report/index.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>报表</div>
+  <div style="padding: 20px">
+    <el-table :data="tableData" style="width: 100%" :max-height="300">
+      <el-table-column fixed prop="date" label="Date" width="150" />
+      <el-table-column prop="name" label="Name" width="120" />
+      <el-table-column prop="state" label="State" width="120" />
+      <el-table-column prop="city" label="City" width="120" />
+      <el-table-column prop="address" label="Address" width="600" />
+      <el-table-column prop="zip" label="Zip" width="120" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default>
+          <el-button link type="primary" size="small" @click="handleClick"
+            >Detail</el-button
+          >
+          <el-button link type="primary" size="small">Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { computed } from "vue";
 
-<style lang="scss"></style>
+const tableHeight = computed(() => {
+  //    return 100vh * 0.9
+});
+
+const handleClick = () => {
+  console.log("click");
+};
+
+const tableData = [];
+
+for (let i = 0; i < 100; i++) {
+  tableData.push({
+    date: "2016-05-03",
+    name: "Tom",
+    state: "California",
+    city: "Los Angeles",
+    address: "No. 189, Grove St, Los Angeles",
+    zip: "CA 90036",
+    tag: "Home",
+  });
+}
+</script>
