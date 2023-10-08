c62132ff604739e07072110f80b9df02db347872
袁隆成 <756885686@qq.com>
2023-10-08 14:05:51 +0800

    wip

--- accounting_electron/src/util/accounting/recording.ts
+++ accounting_electron/src/util/accounting/recording.ts
@@ -10,20 +10,3 @@ export const recording = (name: string, type: string): void => {
   dialogType.value = type;
   dialogName.value = name;
 };
-
-interface Payment {
-  //定义支付/收入方式的接口
-  label: string;
-  value: number;
-}
-export const account: Payment[] = [
-  { label: "微信钱包", value: 1 },
-  { label: "微信零钱通", value: 2 },
-  { label: "支付宝余额", value: 3 },
-  { label: "支付宝余额宝", value: 4 },
-  { label: "银行卡", value: 5 },
-  { label: "基金", value: 6 },
-  { label: "其他", value: 7 },
-  { label: "其他", value: 8 },
-  { label: "其他", value: 9 },
-];
